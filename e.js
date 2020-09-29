953987f43b36188c7aa333054d765ac10ad1d729
Abdelrahimaref <abdoaref163@gmail.com>
2020-09-30 01:20:06 +0200

    tt

--- e.js
+++ e.js
@@ -1,31 +1,35 @@
 var cards = document.querySelectorAll('.card');
-var hasFlippedCard = false ; 
+var i =  0 ; 
 var firstClick , secondClick , exit = 0 ; 
 function memory(){
     this.classList.add('flip');
-    //first Click
-    if(hasFlippedCard == false) {
-        hasFlippedCard = true;
+    if(i == 0) {
+        i++;
         firstClick = this;
-        return;
     }
-    // second click
-    hasFlippedCard = false ; 
-    secondClick = this ;
-    if(firstClick.dataset.color === secondClick.dataset.color && firstClick.dataset.different != secondClick.dataset.different) {
-        firstClick.style.visibility = 'hidden';
-        secondClick.style.visibility = 'hidden';
-        exit++;
-        if(exit == 8) {
-            alert('Congrats !!!');
-            location.reload();
+    else if(i == 1) {
+        secondClick = this ;
+        if(firstClick.dataset.color === secondClick.dataset.color && firstClick.dataset.different != secondClick.dataset.different) {
+            firstClick.style.visibility = 'hidden';
+            secondClick.style.visibility = 'hidden';
+            exit++;
+            if(exit == 8) {
+                alert('Congrats !!!');
+                location.reload();
+            }
         }
-    }
-    else {
-        setTimeout(() => {
-            firstClick.classList.remove('flip');
-            secondClick.classList.remove('flip'); 
-        }, 350);
+        else {
+            setTimeout(() => {
+                firstClick.classList.remove('flip');
+                secondClick.classList.remove('flip'); 
+            }, 350);
+        }
+        i = 0 ;
     }
 }
-cards.forEach(card => card.addEventListener('click',memory))
+cards.forEach( 
+    function(card) {
+        console.log(card);
+        card.addEventListener('click',memory)
+    } 
+)
